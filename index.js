70e33ccb281d3ed693718e9084161cccb9a90ba3
Gustavo <gustavopolonio1@gmail.com>
2023-05-08 10:48:48 -0300

    add: node-cron

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { fauna } from './services/faunadb.js'
 import all from 'faunadb'
 const { query: q } = all
 import dotenv from 'dotenv'
+import cron from 'node-cron'
 
 dotenv.config()
 
@@ -12,12 +13,66 @@ const server = express()
 
 const port = process.env.PORT || 3002
 
-server.get('/', (req, res) => {
-  return res.json({ message: 'Im alive' })
-})
+// async function restartGoals(restartType) {
+//   try {
+//     const response = await fauna.query(
+//       q.Map(
+//         q.Paginate(
+//           q.Documents(
+//             q.Collection("individualGoals")
+//           ),
+//           { size: 100000 }
+//         ),
+//         q.Lambda(
+//           "x",
+//           q.Update(
+//             q.Var("x"),
+//             {
+//               data: { [restartType]: 0 }
+//             }
+//           )
+//         )
+//       )
+//     )
+
+//     return response
+//   } catch(err) {
+//     console.log(err)
+//   }
+// }
+
+// const timer = setInterval(async () => {
+//   const date = new Date()
+//   const dateInBraziliaTimeZone = date.toLocaleString("pt-BR", {
+//     timeZone: "America/Sao_Paulo",
+//     hour: "numeric",
+//     minute: "numeric",
+//     second: "numeric"
+//   })
+
+//   const [hours, minutes, seconds] = dateInBraziliaTimeZone.split(":")
+
+//   if (minutes === '00' && seconds === '00') { // Restart hourly Goals
+//     await restartGoals('avatarHourlyGoals')
+//   }
+
+//   const currentDateInSeconds = (Number(hours) * 60 * 60) + (Number(minutes) * 60) + Number(seconds)
+  
+//   // Round starts at 20:00:00 (Brasilia)
+//   const roundStartInSeconds = 20 * 60 * 60
+
+//   if (currentDateInSeconds === roundStartInSeconds) { // Restart round Goals
+//     await restartGoals('avatarRoundGoals')
+//   }
+
+//   return () => clearTimeout(timer)
+// }, 1000)
 
 
-async function restartGoals(restartType) {
+cron.schedule("* * * * *", async () => {
+  console.log("A cada 1 minuto--")
+  console.log("A cada 1 minuto--")
+
   try {
     const response = await fauna.query(
       q.Map(
@@ -32,7 +87,7 @@ async function restartGoals(restartType) {
           q.Update(
             q.Var("x"),
             {
-              data: { [restartType]: 0 }
+              data: { ['avatarHourlyGoals']: 0 }
             }
           )
         )
@@ -43,34 +98,8 @@ async function restartGoals(restartType) {
   } catch(err) {
     console.log(err)
   }
-}
+})  
 
-const timer = setInterval(async () => {
-  const date = new Date()
-  const dateInBraziliaTimeZone = date.toLocaleString("pt-BR", {
-    timeZone: "America/Sao_Paulo",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric"
-  })
-
-  const [hours, minutes, seconds] = dateInBraziliaTimeZone.split(":")
-
-  if (seconds === '00') { // Restart hourly Goals
-    await restartGoals('avatarHourlyGoals')
-  }
-
-  const currentDateInSeconds = (Number(hours) * 60 * 60) + (Number(minutes) * 60) + Number(seconds)
-  
-  // Round starts at 20:00:00 (Brasilia)
-  const roundStartInSeconds = 20 * 60 * 60
-
-  if (currentDateInSeconds === roundStartInSeconds) { // Restart round Goals
-    await restartGoals('avatarRoundGoals')
-  }
-
-  return () => clearTimeout(timer)
-}, 1000)
 
 server.listen(port, () => {
   console.log("Server initialized")
